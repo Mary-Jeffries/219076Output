f39a6e53c78dc04f947244ddfb177c1bd04ff02e
Mary-Jeffries <60668218+Mary-Jeffries@users.noreply.github.com>
2020-02-25 08:55:41 -0500

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _219076Output
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            //Don't put code here
+            InitializeComponent();
+            //Code here is ok - will only run when the window is created
+            Console.WriteLine("hello");
+
+            //This is a message box
+            MessageBox.Show("I am a message box text!",
+                "I am a caption",
+                MessageBoxButton.YesNoCancel,
+                MessageBoxImage.Warning);
+
+            Label mylabel = new Label();
+            mylabel.Content = "Whoo hoo - my new label";
+            spStack.Children.Add
+        }  
+    }
+}
